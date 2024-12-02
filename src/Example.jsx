b2484fe994fd242711028985ee5e901313ba14a5
Yoon Me Me <yoonmeme.engr@gmail.com>
2024-12-02 21:46:42 +0630

    first commit

--- src/Example.jsx
+++ src/Example.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+
+// const Example = () => {
+//     const [text, setText] = useState("Hello World");
+//     const textChange = (e) => {
+//         setText(e.target.value);
+//     }
+//   return (
+//     <div>
+//         <h1>{text}</h1>
+//         {/* <input value={text} onChange={(e) => setText(e.target.value)} type="text" /> */}
+//         <input value={text} onChange={textChange} type="text" />
+//     </div>
+//   )
+// }
+
+// export default Example
+
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {} from '@fortawesome/free-solid-svg-icons'
 
 const Example = () => {
-    const [text, setText] = useState("Hello World");
-    const textChange = (e) => {
-        setText(e.target.value);
-    }
   return (
     <div>
-        <h1>{text}</h1>
-        {/* <input value={text} onChange={(e) => setText(e.target.value)} type="text" /> */}
-        <input value={text} onChange={textChange} type="text" />
+      <button>
+        <FontAwesomeIcon icon="fa-solid fa-bars" />
+      </button>
     </div>
   )
 }
