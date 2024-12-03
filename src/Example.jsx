659974cd8bfa7c77ea15c0225daabb7b3336e78e
Yoon Me Me <yoonmeme.engr@gmail.com>
2024-12-03 13:23:38 +0630

    first commit

--- src/Example.jsx
+++ src/Example.jsx
@@ -127,11 +127,39 @@
 // export default Example
 
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+import './Users.css'
 
 const Example = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    const fetchUsers = async()=>{
+      const response = await fetch('https://api.github.com/users');
+      const data = await response.json();
+      setUsers(data);
+      console.log(users);
+    }
+    fetchUsers();
+  },[])
+
   return (
-    <div>
-      
+    <div className='user-list'>
+      <h1>GitHub User Lists</h1>
+      <ul>
+        {users.map(user=>(
+          <li key={user.id}>
+            <img src={user.avatar_url} alt={user.login} />
+            <div className="">
+              <p>{user.login}</p>
+              <a href={user.html_url}>
+                <button>GitHub Repo</button>
+              </a>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
